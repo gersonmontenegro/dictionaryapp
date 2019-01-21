e180b49cf926680151ff7a2ca224be7453c54579
Gerson Montenegro <montenegrogerson@gmail.com>
2019-01-21 16:23:05 -0500

    First prototype of SearchScreen

--- src/screens/search/index.js
+++ src/screens/search/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { View, Text } from 'react-native';
+import { FlatList, View, Text, TextInput } from 'react-native';
 
 class SearchScreen extends PureComponent {
     constructor(props) {
@@ -10,11 +10,30 @@ class SearchScreen extends PureComponent {
     render() {
         return (
             <View style={{ flex: 1 }}>
-                <View style={{ height: 40, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ color: 'white', fontSize: 18 }}>Search</Text>
+                <View style={{ height: 40, backgroundColor: 'lighgray', justifyContent: 'center', alignItems: 'center' }}>
+                    <Text style={{ color: 'black', fontSize: 18, fontFamily: 'Prata-Regular' }}>Search</Text>
                 </View>
-                <View style={{ flex: 1, backgroundColor: 'lighgray' }}>
-
+                <View style={{ height: 38, backgroundColor: 'lighgray', marginTop: 2 }}>
+                    <TextInput style={{ height: 50, backgroundColor: 'white', fontFamily: 'Prata-Regular', borderColor: 'black', borderWidth: 1 }} placeholder="Search..." />
+                </View>
+                <View style={{ backgroundColor: 'white', flex: 1, marginTop: 15 }}>
+                    <View style={{ height: 30, backgroundColor: 'gray', justifyContent: 'center' }}>
+                        <Text style={{ fontFamily: 'Prata-Regular', marginLeft: 5 }}>Results</Text>
+                    </View>
+                    <FlatList
+                        data={[{ key: 'a' }, { key: 'b' }, { key: 'c' }]}
+                        renderItem={({ item }) => {
+                            return (
+                                <View style={{ height: 40, justifyContent: 'center', borderBottomColor: 'gray', borderBottomWidth: 1 }}>
+                                    <View style={{ flexDirection: 'row' }}>
+                                        <Text style={{ fontFamily: 'Prata-Regular', marginLeft: 5, color: 'black' }}>forever</Text>
+                                        <Text style={{ fontFamily: 'Prata-Regular', marginLeft: 5, color: 'gray' }}>adverb</Text>
+                                    </View>
+                                </View>
+                            );
+                        }
+                        }
+                    />
                 </View>
             </View>
         );
