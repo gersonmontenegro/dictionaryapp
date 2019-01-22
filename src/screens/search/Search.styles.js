faff224d015f73bf072c4c0e584341909d3e7880
Gerson Montenegro <montenegrogerson@gmail.com>
2019-01-22 10:51:18 -0500

    Creating styles to SearchScreen

---
+++ src/screens/search/Search.styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const searchStyles = StyleSheet.create({
+    mainContainer: {
+        flex: 1
+    },
+    headerContainer: {
+        height: 40,
+        backgroundColor: '#eaeaea',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textHeader: {
+        color: 'black',
+        fontSize: 18,
+        fontFamily: 'Prata-Regular'
+    }
+});
