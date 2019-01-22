5fe1492ba1b3784b1b44a1077ebfec27d3f9cd8f
Gerson Montenegro <montenegrogerson@gmail.com>
2019-01-22 16:04:26 -0500

    Implementing SearchResultsTitle component

--- src/screens/search/SearchResults.js
+++ src/screens/search/SearchResults.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native'
 import SearchResultsList from 'src/screens/search/SearchResultsList';
+import SearchResultsTitle from './SearchResultsTitle';
 
 const SearchResults = () => {
     return (
         <View style={{ backgroundColor: 'white', flex: 1, marginTop: 15 }}>
-            <View style={{ height: 30, backgroundColor: 'gray', justifyContent: 'center' }}>
-                <Text style={{ fontFamily: 'Prata-Regular', marginLeft: 5 }}>Results</Text>
-            </View>
+            <SearchResultsTitle title="Results" />
             <SearchResultsList />
         </View>
     );
