3572198e94893fa20b2a6ebbf4ccd5ca83b86f11
Gerson Montenegro <montenegrogerson@gmail.com>
2019-01-25 11:30:23 -0500

    Creating Definition component

---
+++ src/screens/detail/Definition.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Definition = ({ id, definition, example }) => {
+    return (
+        <View>
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={{ width: 20, fontSize: 12, fontWeight: 'bold' }}>{id}</Text>
+                <Text style={{ fontFamily: 'Prata-Regular' }}>{definition}</Text>
+            </View>
+            <Text style={{ fontWeight: 'bold', marginLeft: 15, fontFamily: 'Prata-Regular' }}>{example}</Text>
+        </View>
+    );
+}
+
+export default Definition;
