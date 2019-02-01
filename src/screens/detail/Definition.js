a9bf7e6afaef5308775c44f05dfdbd19a61cd1b4
Gerson Montenegro <montenegrogerson@gmail.com>
2019-01-31 21:56:23 -0500

    Just an example of how to load a definition. This is a test.

--- src/screens/detail/Definition.js
+++ src/screens/detail/Definition.js
@@ -13,4 +13,25 @@ const Definition = ({ id, definition, example }) => {
     );
 }
 
+function loadDict() {
+    let url = "https://od-api.oxforddictionaries.com/api/v1/entries/en/forever/regions=us";
+    let options = {
+        headers: { "app_id": "9d8a319a", "app_key": "5e52771af8525d683fafa9353a4ccab8" },
+        method: 'GET'
+    }
+    fetch(url, options)
+        .then(r => {
+            if (r.status === 200) {
+                return r.json();
+            } else {
+                console.log("Error fetching...");
+            }
+        })
+        .then(data => {
+            console.log(data);
+        }).catch(e => {
+            console.log("E", JSON.stringify(e));
+
+        });
+}
 export default Definition;
