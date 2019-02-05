3fc46680cf35389633f196d1f934f778a6729b01
Gerson Montenegro <montenegrogerson@gmail.com>
2019-02-05 10:08:25 -0500

    Implemeting first release of the search functionality to SearchInput component

--- src/screens/search/SearchInput.js
+++ src/screens/search/SearchInput.js
@@ -1,32 +1,59 @@
 import React, { PureComponent } from 'react';
-import { View, TextInput } from 'react-native';
+import { View, ToastAndroid } from 'react-native';
 import { searchStyles } from 'src/screens/search/Search.styles';
 import SearchWord from 'src/providers/';
 import { SearchBar } from 'react-native-elements';
+import SearchButton from 'src/screens/search/SearchButton';
 
 class SearchInput extends PureComponent {
     constructor(props) {
         super(props);
-        let s = new SearchWord();
-        s.initSearch('forever');
-        this.state = { 'searchWord': '' };
+        this.search = new SearchWord();
+        this.state = {
+            searchWord: '',
+            showLoading: false
+        };
     }
 
     updateSearch = search => {
         this.setState({ 'searchWord': search });
     }
 
+    addSearchButton = () => <SearchButton onSearchButtonClick={this.onSearchButtonClick} />
+
+    onSearchButtonClick = async () => {
+        let result = '';
+        if (this.state.searchWord.length) {
+            result = await this.search.initSearchAsync(this.state.searchWord);
+            if (result) {
+                console.log("good!");
+            } else {
+                this.showToast(`No results for "${this.state.searchWord}"`);
+            }
+        }
+    }
+
+    showToast = (message) => {
+        ToastAndroid.showWithGravity(message,
+            ToastAndroid.LONG,
+            ToastAndroid.BOTTOM,
+            25,
+            50);
+    }
+
     render() {
         return (
-            <View style={searchStyles.inputContainer}>
+            <View style={[searchStyles.inputContainer]}>
                 <SearchBar
                     placeholder="Search......"
                     inputStyle={searchStyles.inputStyle}
                     value={this.state.searchWord}
                     onChangeText={this.updateSearch}
                     containerStyle={{ backgroundColor: 'white' }}
+                    showLoading={this.state.showLoading}
+                    searchIcon={this.addSearchButton}
                 />
-            </View>
+            </View >
         );
     }
 }
