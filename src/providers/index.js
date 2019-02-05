ed1b60078b0bffc00efc7fdc7d237226c1cf4acf
Gerson Montenegro <montenegrogerson@gmail.com>
2019-02-05 10:07:42 -0500

    Creating async version in order to search a word

--- src/providers/index.js
+++ src/providers/index.js
@@ -19,6 +19,16 @@ class SearchWord {
             .catch(this.onCatch);
     }
 
+    async initSearchAsync(word) {
+        let url = `https://od-api.oxforddictionaries.com/api/v1/entries/en/${word}/regions=us`;
+        let options = {
+            headers: { "app_id": app_id, "app_key": app_key },
+            method: 'GET'
+        }
+        let response = await fetch(url, options);
+        return response.status === 200 ? response.json() : false;
+    }
+
     onCatch(error) {
         console.debug("E", JSON.stringify(error));
     }
