e339dfbbd05912f732fb0552d4620367319bd22b
Gerson Montenegro <montenegrogerson@gmail.com>
2019-02-08 22:28:59 -0500

    Implementing redux provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { PureComponent } from 'react';
 import { View } from 'react-native';
-import { SplashScreen } from 'src/screens/splash';
+import { Provider } from 'react-redux';
 import { Styles } from './assets/style/Styles';
 import MainNavigator from 'src/navigation';
+import store from 'src/store';
 
 export default class App extends PureComponent {
   render() {
     return (
-      <View style={Styles.MainContainer}>
-        <MainNavigator />
-      </View>
+      <Provider store={store}>
+        <View style={Styles.MainContainer}>
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
