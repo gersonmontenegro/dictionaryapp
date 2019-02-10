26730f1d853b2c5959f103234450dc2f93fa8d36
Gerson Montenegro <montenegrogerson@gmail.com>
2019-02-10 15:23:57 -0500

    Implementing redux on the results header in order to show the results quantity

--- src/screens/search/SearchResultsTitle.js
+++ src/screens/search/SearchResultsTitle.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { searchStyles } from 'src/screens/search/Search.styles';
+import { connect } from 'react-redux';
 
-const SearchResultsTitle = ({ title }) => {
+const SearchResultsTitle = ({ title, results }) => {
     return (
         <View style={searchStyles.searchResultsTitleContainer}>
-            <Text style={searchStyles.searchResultsTitle}>{title}</Text>
+            <Text style={searchStyles.searchResultsTitle}>{title} {printResultsQuantity(results.words)}</Text>
         </View>
     );
 }
 
-export default SearchResultsTitle;
+printResultsQuantity = (data) => {
+    return data.length ? data.length : '';
+}
+
+const mapStateToProps = (state) => {
+    const { results } = state;
+    return { results };
+}
+
+export default connect(mapStateToProps)(SearchResultsTitle);
