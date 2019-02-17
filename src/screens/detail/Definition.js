8140a712e62c54a8cf2eefddbf2402e3d65316ad
Gerson Montenegro <montenegrogerson@gmail.com>
2019-02-17 00:36:41 -0500

    Chaging names for the real ones

--- src/screens/detail/Definition.js
+++ src/screens/detail/Definition.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Definition = ({ id, definition, example }) => {
+const Definition = ({ id, definitions, examples }) => {
     return (
         <View key={id}>
             <View style={{ flexDirection: 'row' }}>
