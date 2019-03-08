4811cd1144141928693d577bdc8393b7212ee180
Gerson Montenegro <montenegrogerson@gmail.com>
2019-03-07 23:56:53 -0500

    Adding elements to a more complete header

--- src/components/GenericHeader.js
+++ src/components/GenericHeader.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight, Image } from 'react-native';
 import { searchStyles } from 'src/screens/search/Search.styles';
 
 const GenericHeader = ({ title }) => {
     return (
         <View style={searchStyles.headerContainer}>
-            <Text style={searchStyles.textHeader}>{title}</Text>
+            <View style={{ flex: 1 }}>
+                <TouchableHighlight>
+                    <Image style={{ height: 30, width: 30 }} source={require('assets/img/lett_arrow.png')} />
+                </TouchableHighlight>
+            </View>
+            <View style={{ flex: 5, alignSelf: 'center' }}>
+                <Text style={searchStyles.textHeader}>{title}</Text>
+            </View>
+            <View style={{ flex: 1 }}>
+
+            </View>
         </View>
     );
 }
