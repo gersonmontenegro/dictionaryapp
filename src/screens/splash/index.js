056cf2226edae3c8d6d3d83074b2176399be1ed7
Gerson Montenegro <montenegrogerson@gmail.com>
2019-03-10 13:37:14 -0500

    Jumping from Splash to Search screen after 2 seconds

--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { View } from 'react-native';
 import LogoComponent from './LogoApp';
 import { SplashStyle } from './Splash.styles';
+import { withNavigation } from 'react-navigation';
 
-const SplashScreen = () => {
+const SplashScreen = ({ navigation }) => {
     return (
         <View style={SplashStyle.SplashContainer}>
             <LogoComponent />
+            {
+                jumtoToSearch(navigation)
+            }
         </View>
     );
 }
 
-export default SplashScreen;
+jumtoToSearch = (navigation) => {
+    setTimeout(() => {
+        navigation.navigate('Search');
+    }, 2000);
+}
+
+export default withNavigation(SplashScreen);
